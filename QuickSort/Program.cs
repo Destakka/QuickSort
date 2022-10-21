596a9f1f195f60779370c2800ba5e225ac8d7a13
Destakka <d.atikananda.ft21@mail.umy.ac.id>
2022-10-21 17:14:18 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of the integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        //Number of elements in array
+        private int n;
+
+    }
+}
